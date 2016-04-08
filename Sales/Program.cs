907a2375944c3970b93b7b204a590724b5c01fc1
Anthony Fry <antfry256@gmail.com>
2016-04-08 15:31:23 +1000

    Added MySQL Connection

---
+++ Sales/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace Sales
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+
+
+            string connString = "Server=127.0.0.1;Port=3306;Database=sales;Uid=root;password=alpine12";
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "SELECT ItemName FROM inventory where ItemID=1";
+
+
+            try
+            {
+                conn.Open();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            MySqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader["ItemName"].ToString());
+            }
+
+            Console.ReadLine();
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+
+}
