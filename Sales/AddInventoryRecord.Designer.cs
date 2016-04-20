adc5d177783ee5c02e2da1d04da572e8b4ac841c
Anthony Fry <antfry256@gmail.com>
2016-04-20 23:01:42 +1000

    Added Descriptions of Objects

--- Sales/AddInventoryRecord.Designer.cs
+++ Sales/AddInventoryRecord.Designer.cs
@@ -34,8 +34,6 @@ namespace Sales
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
@@ -59,7 +57,7 @@ namespace Sales
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(116, 380);
+            this.button2.Location = new System.Drawing.Point(118, 305);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(184, 53);
             this.button2.TabIndex = 1;
@@ -67,26 +65,9 @@ namespace Sales
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.btnConfirm);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(171, 131);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(135, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 134);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Stock ID:";
-            this.label1.Click += new System.EventHandler(this.lblStockID);
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(171, 173);
+            this.textBox2.Location = new System.Drawing.Point(167, 148);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(135, 20);
             this.textBox2.TabIndex = 4;
@@ -95,7 +76,7 @@ namespace Sales
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(96, 176);
+            this.label2.Location = new System.Drawing.Point(92, 151);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 13);
             this.label2.TabIndex = 5;
@@ -104,7 +85,7 @@ namespace Sales
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(171, 214);
+            this.numericUpDown1.Location = new System.Drawing.Point(167, 189);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(61, 20);
             this.numericUpDown1.TabIndex = 6;
@@ -112,7 +93,7 @@ namespace Sales
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(171, 254);
+            this.textBox3.Location = new System.Drawing.Point(167, 229);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(135, 20);
             this.textBox3.TabIndex = 7;
@@ -121,7 +102,7 @@ namespace Sales
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(85, 216);
+            this.label3.Location = new System.Drawing.Point(81, 191);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(80, 13);
             this.label3.TabIndex = 8;
@@ -131,7 +112,7 @@ namespace Sales
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(85, 257);
+            this.label4.Location = new System.Drawing.Point(81, 232);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(65, 13);
             this.label4.TabIndex = 9;
@@ -155,7 +136,7 @@ namespace Sales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 586);
+            this.ClientSize = new System.Drawing.Size(437, 429);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -163,8 +144,6 @@ namespace Sales
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "AddInventoryRecord";
@@ -181,8 +160,6 @@ namespace Sales
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
